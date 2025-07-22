3658cced5e75ff4d0a913ed4d7627c46cbb476a0
Sarath <learn.cypher@gmail.com>
2025-07-22 16:22:29 +0530

    Resolved merge conflicts

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from flask import request
 
 app = Flask(__name__)
 
@@ -6,31 +7,29 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+
 @app.route('/user/<username>')
 def show_user(username):
     return f"<h2> Hello, {username} </h2>"
 
-from flask import request
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         user = request.form['username']
         return f"Welcome, {user}"
-    return '''
-        <form method="post">
-            Username: <input name="username">
-            <input type="submit">
-        </form>
-    '''
+    return render_template('login.html')
+
 
 @app.route('/add_users')
 def addUsers():
     return render_template('add_users.html')
 
+
 @app.route('/users')
 def user():
     return render_template('users.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
